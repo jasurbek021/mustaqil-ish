91c6ea0493874c2097c3360a4ee90ec598f7b187
jasur@gmail.com <jasur@gmail.com>
2024-04-20 00:12:45 +0500

    mustaqil ish

---
+++ app.js
@@ -0,0 +1,6 @@
+let ism = prompt("ismingizn kriting");
+let birinchiharif = ism.charAt().toUpperCase();
+let davom = ism.slice(1).toLowerCase();
+let result = birinchiharif + davom;
+
+alert(`salom, ${result}`);
